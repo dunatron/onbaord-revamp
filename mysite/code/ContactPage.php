afb428ca0f01f1acaa4a0870d9e4933fb39c8097
Dunatron <heath.dunlop.hd@gmail.com>
2016-02-07 13:19:35 +1300

    contact-default form

--- mysite/code/ContactPage.php
+++ mysite/code/ContactPage.php
@@ -6,35 +6,76 @@
  * Time: 6:16 PM
  */
 
+//class ContactPage extends Page {
+//
+//}
+//
+//class ContactPage_Controller extends Page_Controller {
+//
+//    private static $allowed_actions = array('ContactForm');
+//
+//    // Contact form to use in template($ContactForm)
+//    public function ContactForm() {
+//        $fields = new FieldList(
+//            new TextField('Name'),
+//            new EmailField('Email'),
+//            new TextareaField('Message')
+//        );
+//
+//        $actions = new FieldList(
+//            new FormAction('submit', 'Submit')
+//        );
+//
+//        $validator = new RequiredFields('Name', 'Message');
+//
+//        return new Form($this, 'Form', $fields, $actions, $validator);
+//    }
+//
+//    public function submit($data, $form) {
+//        $email = new Email();
+//
+//        $email->setTo('heath.dunlop.hd@gmail.com');
+//        $email->setFrom($data['Email']);
+//        $email->setSubject("Contact Message from {$data["Name"]}");
+//
+//        $messageBody = "
+//            <p><strong>Name:</strong> {$data['Name']}</p>
+//            <p><strong>Message:</strong> {$data['Message']}</p>
+//        ";
+//
+//        $email->setBody($messageBody);
+//        $email->send();
+//        return array(
+//            'Content' => '<p>Thank you for your feedback.',
+//            'ContactForm' => ''
+//        );
+//    }
+//
+//}
+
 class ContactPage extends Page {
-
 }
-
 class ContactPage_Controller extends Page_Controller {
 
-    private static $allowed_actions = array('ContactForm');
+    private static $allowed_actions = array('Form');
 
-    // Contact form to use in template($ContactForm)
-    public function ContactForm() {
+    public function Form() {
         $fields = new FieldList(
             new TextField('Name'),
             new EmailField('Email'),
             new TextareaField('Message')
         );
-
         $actions = new FieldList(
             new FormAction('submit', 'Submit')
         );
-
         $validator = new RequiredFields('Name', 'Message');
-
         return new Form($this, 'Form', $fields, $actions, $validator);
     }
 
     public function submit($data, $form) {
         $email = new Email();
 
-        $email->setTo('heath.dunlop.hd@gmail.com');
+        $email->setTo('siteowner@mysite.com');
         $email->setFrom($data['Email']);
         $email->setSubject("Contact Message from {$data["Name"]}");
 
@@ -42,13 +83,13 @@ class ContactPage_Controller extends Page_Controller {
             <p><strong>Name:</strong> {$data['Name']}</p>
             <p><strong>Message:</strong> {$data['Message']}</p>
         ";
-
         $email->setBody($messageBody);
         $email->send();
         return array(
-            'Content' => '<p>Thank you for your feedback.',
-            'ContactForm' => ''
+            'Content' => '<p>Thank you for your feedback.</p>',
+            'Form' => ''
         );
     }
-
 }
+
+
--- themes/onboard/templates/Layout/ContactPage.ss
+++ themes/onboard/templates/Layout/ContactPage.ss
@@ -1,3 +1,4 @@
 <div class="container">
-    $ContactForm
+    <%--$ContactForm--%>
+    $Form
 </div>
